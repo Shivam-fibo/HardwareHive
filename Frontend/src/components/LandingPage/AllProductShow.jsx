3ffd2959cbc3e4ac69fc4980923c21b0873f26a1
Shivam <sg641818@gmail.com>
2025-03-06 12:15:52 +0530

    show notifaction on clicking anywhere in all product page

--- Frontend/src/components/LandingPage/AllProductShow.jsx
+++ Frontend/src/components/LandingPage/AllProductShow.jsx
@@ -1,10 +1,23 @@
 import Footer from "../Home/Footer";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 
 export default function ProductGrid() {
     const images = Array.from({ length: 15 }, (_, i) => `/images/img${i + 1}.jpg`);
+    useEffect(() => {
+      const handleClick = () => {
+        toast.error("Please login or register first!");
+      };
   
+      document.addEventListener("click", handleClick);
+  
+      return () => {
+        document.removeEventListener("click", handleClick);
+      };
+    }, []);
     return (
         <div className="p-6 bg-gray-900">
+          
       <h2 className="text-2xl sm:text-3xl font-bold text-center text-gray-100 mb-6">Our Products</h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         {images.map((src, index) => (

4909336357441e656181685b500a47f098696a35
Shivam <sg641818@gmail.com>
2025-05-24 14:16:36 +0530

    Update Nabar component: Add text-nowrap class to My Account dropdown

--- Frontend/src/components/Home/Nabar.jsx
+++ Frontend/src/components/Home/Nabar.jsx
@@ -82,7 +82,7 @@ function Header() {
                 ref={profileRef}
                 className="absolute border-gray-500 w-32 top-10 sm:top-11 right-4 sm:right-8 bg-white text-black shadow-lg rounded-lg z-50 overflow-hidden text-sm font-medium"
               >
-                <p onClick={() => navigate("/user")} className="cursor-pointer hover:bg-gray-300 flex items-center gap-2 px-4 p-1.5">
+                <p onClick={() => navigate("/user")} className="cursor-pointer hover:bg-gray-300 flex items-center gap-2 px-4 p-1.5 text-nowrap">
                   <FaRegUser size={12} /> My Account
                 </p>
                 <p onClick={() => navigate("/")} className="cursor-pointer hover:bg-gray-300 flex items-center gap-2 px-4 p-1.5">

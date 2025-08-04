c5bd3d97cf58d72a687db34d6d4e4ee000321747
synoize <suryavanshivam@gamil.com>
2025-08-04 10:00:34 +0530

    update header

--- Frontend/src/components/LandingPage/Module/Header.jsx
+++ Frontend/src/components/LandingPage/Module/Header.jsx
@@ -1,32 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { FaUserLock } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa6";
+import { LuUsers } from "react-icons/lu";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+
   const navigate = useNavigate();
 
-  const handleProduct = (e) => {
-    e.stopPropagation(); 
-    console.log("handle Product clicked");
-    navigate("/allProductShow");
-  };
+  useEffect(() => {
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setOpen(false);
+        }
+      }
+  
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, []);
 
   return (
 
-    <div className="bg-white p-2">
+    <header className="bg-white p-2 sm:h-12 flex justify-between items-center sm:mx-6">
 
-      <header
-        className="w-full h-full flex justify-between items-center cursor-pointer"
-        onClick={() => navigate("/")}
+      <div onClick={() => navigate("/")}
+        className="h-full flex justify-between items-center cursor-pointer"
       >
-        <img src="/logo/ss_power_tool_logo.svg" width={"150px"} className="sm:ml-6" />
-        <button
-          className="w-[100px] h-[35px] sm:mr-6 bg-[#013E70] rounded-xl border border-[#013E70] text-white cursor-pointer"
-          onClick={handleProduct} 
+        <img src="/logo/ss_power_tool_logo.svg" width={"150px"} />
+      </div>
+
+      <span onClick={() => setOpen(!open)}>
+        <FaUserLock className="text-xl" />
+      </span>
+
+      {open && (
+        <div
+          ref={ref}
+          className="absolute border-gray-500 top-10 sm:top-11 right-4 sm:right-8 bg-white text-black shadow-lg rounded-lg z-50 overflow-hidden text-sm font-medium"
         >
-          Products
-        </button>
-      </header>
-    </div>
+          <p onClick={() => navigate("/login")} className="cursor-pointer hover:bg-gray-300 flex items-center gap-2 px-4 p-1.5">
+            <FaRegUser size={14} /> Login
+          </p>
+          <p onClick={() => navigate("/register")} className="cursor-pointer hover:bg-gray-300 flex items-center gap-2 px-4 p-1.5">
+            <LuUsers size={14} /> Sign Up
+          </p>
+        </div>
+      )}
+    </header>
   );
 };
 
